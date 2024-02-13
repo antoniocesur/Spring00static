1471217f53ee9c9d585f427489ea96866b000d88
AntonioSalinasGarcía <antonio.salinas@ext.cesurformacion.com>
2024-02-13 10:00:53 +0100

    3

---
+++ src/test/java/com/example/Spring00static/Spring00staticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring00static;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring00staticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
